847c5bfac0c26e288c84e958abc342e545467dd6
mobinghasemi <mobinghasemikurd@gmail.com>
2024-08-23 21:59:00 +0330

    adding shell scripts to the root folder

---
+++ sh.sh
@@ -0,0 +1,9 @@
+torch-model-archiver --model-name mnist --version 2.0 --model-file mnist.py --serialized-file mnist_cnn.pt --handler mnist_handler_base.py --force
+
+mv mnist.mar model-store/
+
+docker run --rm -it -p 8080:8080 -p 8081:8081 --name mar -v $(pwd)/model-store:/home/model-server/model-store -v $(pwd):/home/model-server/examples pytorch/torchserve:latest torchserve --start --model-store model-store --models mnist=mnist.mar
+
+curl http://127.0.0.1:8081/models/
+
+curl http://127.0.0.1:8080/predictions/mnist -T 3.png
