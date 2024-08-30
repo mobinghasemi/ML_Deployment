de2fa2c024c3d3d877079c7eee7247a7c7340029
mobinghasemi <mobinghasemikurd@gmail.com>
2024-08-30 12:40:52 +0330

    modifying sh.sh files to correct form for this project

--- sh.sh
+++ sh.sh
@@ -1,4 +1,4 @@
-torch-model-archiver --model-name mnist --version 2.0 --model-file mnist.py --serialized-file mnist_cnn.pt --handler mnist_handler_base.py --force
+torch-model-archiver --model-name mnist --version 2.0 --model-file arch.py --serialized-file mnist.pth --handler mnist_handler_base.py --force
 
 mv mnist.mar model-store/
 
